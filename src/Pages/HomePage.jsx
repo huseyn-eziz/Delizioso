457413d0dd7a5a90201e7f61298c9d143a5c56af
huseyn-eziz <mastereziz@gmail.com>
2023-12-06 02:07:51 +0400

    easy pagination addded

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,5 @@
 import Cards from "../components/Cards/Cards";
-import CardsWithPagination from "../components/Cards/CardsWithPagination";
-import Pagination from "../components/Cards/CardsWithPagination";
-import Page from "../components/Cards/CardsWithPagination";
 import Header from "../components/Header/Header";
-import PageContainer from "../components/PageContainer/PageContainer";
 import Restauran from "../components/Restauran/Restauran";
 import Welcome from "../components/Welcome/Welcome";
 
@@ -14,8 +10,7 @@ const HomePage = () => {
       <Header />
       <Restauran />
       <Welcome />
-      <CardsWithPagination />
-
+      <Cards />
     </>
   );
 };
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,38 +1,61 @@
-import axios from "axios";
-import { useState } from "react";
-import { useEffect } from "react";
+import axios from "axios"
+import { useEffect, useState } from "react"
 
-function Cards({ page, setTotalPages }) {
-    const [cards, setCards] = useState([]);
+
+const Cards = () => {
+    const [cards, setCards] = useState([])
+    const [page, setPage] = useState(1)
+    const [itemsForpage, setItemsForpage] = useState(3)
+
     const url = "http://localhost:3000/cards"
     useEffect(() => {
         axios.get(url).then(({ data }) => {
             setCards(data)
         })
-        setTotalPages(Math.ceil(cards.length / NumberOfShowCards))
 
     }, []);
 
-    const NumberOfShowCards = 3;
+    const totalPages = Math.ceil(cards.length / itemsForpage)
 
-    const startIndex = (page - 1) * NumberOfShowCards;
-    const selectedCards = cards.slice(startIndex, startIndex + NumberOfShowCards);
+    const currentData = cards.slice(
+        (page - 1) * itemsForpage,
+        page * itemsForpage
 
+    );
+
+    const handlePageChange = (page) => {
+        setPage(page);
+    }
 
 
     return (
-        <div className="container mx-auto mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {selectedCards.map(({ id, title,     description }) => (
-                <div key={id} className="bg-white p-4 rounded-md shadow-md">
-                    <h3 className="text-xl font-bold mb-2">{title}</h3>
-                    <p className="text-gray-700">{description}</p>
+        <>
+            <div className=" container mx-auto mt-8 p-4 bg-gray-100 rounded-md shadow-md">
+                <h5 className="text-center text-lg font-bold mb-4 text-red-500 px-4 py-2 rounded-md bg-red-200 hover:bg-gray-00">Page: {page}</h5>
+                <div className="container mx-auto mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        
+                {currentData.map(({ id, title, description }) => (
+                    <div key={id} className="bg-white p-4 rounded-md shadow-md">
+                        <h3 className="text-xl font-bold mb-2">{title}</h3>
+                        <p className="text-gray-700">{description}</p>
+                    </div>
+                ))}
                 </div>
-            ))}
-        </div>
 
+                <div className="flex justify-center gap-4 mt-4"> 
+                    {Array.from({ length: totalPages }, (_, index) => (
+                        <button  className={`text-red-500 px-4 py-2 rounded-md focus:outline-none transition duration-300 ease-in-out 
+                        ${page === index + 1 ? 'bg-red-500 text-white' : 'bg-white hover:bg-gray-200 hover:text-gray-800'}`}
+                   
+                        key={index + 1} onClick={() => handlePageChange(index + 1)}>
+                            {index + 1}
+                        </button>
+                    ))}
+                </div>
+            </div>
+
+        </>
     )
 }
 
 export default Cards
-
-
